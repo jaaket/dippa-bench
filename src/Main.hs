4696259526ffa20a41ccfd93f4ed9262b4004c46
Jaakko Ketola <jaaket@kapsi.fi>
2016-03-31 17:50:56 +0300

    plotting

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveFoldable    #-}
 {-# LANGUAGE DeriveFunctor     #-}
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -14,19 +13,13 @@ import           Criterion.Types                 (Report (..),
                                                   SampleAnalysis (..))
 import qualified Data.ByteString.Lazy            as B
 import qualified Data.Csv                        as Csv
+import           Data.List                       (transpose)
 import qualified Data.Text                       as T
-import           GHC.Generics                    (Generic)
 import           Statistics.Resampling.Bootstrap (Estimate (..))
 
 import qualified Freer
 import qualified Mtl
 
-
-data BenchElem label info = BenchElem {
-      elemLabel :: label
-    , elemInfo  :: info
-    }
-    deriving (Functor, Show, Generic)
 
 data Bench a = Bench {
       benchDescription :: T.Text
@@ -37,35 +30,31 @@ data Bench a = Bench {
 data BenchGroup a = BenchGroup {
       bgDescription :: T.Text
     , bgBenches     :: [Bench a]
+    , bgXAxis       :: [Double]
     }
     deriving (Functor, Show, Foldable, Traversable)
 
 countdown = BenchGroup {
       bgDescription = "countdown"
     , bgBenches = map (\(name, benchmark) ->
-            Bench name (map (\n -> BenchElem n (whnf benchmark n)) xValues)
+            Bench name (map (whnf benchmark) xValues)
           )
           [("freer", Freer.countdown), ("mtl", Mtl.countdown)]
+    , bgXAxis = map fromIntegral xValues
     }
   where
-    xValues = steps (10^3) (10^2) 3
+    xValues = steps (10^7) (10^6) 5
 
-runBench :: BenchElem a Benchmarkable -> IO (BenchElem a Report)
-runBench (BenchElem label bench) = BenchElem label <$> benchmark' bench
+runBenchGroup :: BenchGroup Benchmarkable -> IO (BenchGroup Report)
+runBenchGroup = mapM benchmark'
 
-getPlotData :: BenchElem a Report -> BenchElem a Double
-getPlotData = fmap (estPoint . anMean . reportAnalysis)
+getMean:: Report -> Double
+getMean = estPoint . anMean . reportAnalysis
 
-data BenchRow a b = BenchRow T.Text a b deriving (Show, Generic)
-
-instance (Csv.ToField a, Csv.ToField b) => Csv.ToRecord (BenchRow a b)
-
-benchRows :: Bench (BenchElem a b) -> [BenchRow a b]
-benchRows (Bench desc rows) = map (\(BenchElem x y) -> BenchRow desc x y) rows
-
-benchGroupCsv :: Csv.ToField a => BenchGroup (BenchElem a Report) -> B.ByteString
-benchGroupCsv (BenchGroup _ benches) =
-    Csv.encode (concatMap (benchRows . fmap getPlotData) benches)
+benchGroupCsv :: BenchGroup Report -> B.ByteString
+benchGroupCsv (BenchGroup _ results xAxis) =
+    Csv.encode ["time" : map benchDescription results] `mappend`
+    Csv.encode (zipWith (:) xAxis (transpose $ map (map getMean . benchData) results))
 
 steps :: Num a => a -> a -> Int -> [a]
 steps _ _ 0 = []
@@ -73,5 +62,5 @@ steps low stepSize n = low : steps (low + stepSize) stepSize (n - 1)
 
 main :: IO ()
 main = do
-    result <- mapM runBench countdown
-    print (benchGroupCsv result)
+    result <- runBenchGroup countdown
+    B.writeFile "test.csv" (benchGroupCsv result)
