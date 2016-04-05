8bc63e13ae4ad9ce48eb11e7e1abb0011bad41c6
Jaakko Ketola <jaaket@kapsi.fi>
2016-04-05 09:51:26 +0300

    Use Cont instead of Amb in Mtl.nQueens, much faster

--- src/Mtl/NQueens.hs
+++ src/Mtl/NQueens.hs
@@ -1,18 +1,30 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Mtl.NQueens where
 
-import           Control.Monad.Amb
+import           Control.Applicative (Alternative, empty, (<|>))
+import           Control.Monad.Cont
 
 import           Common.NQueens
 
 
-addQueens :: Int -> Int -> [(Int, Int)] -> Amb [(Int, Int)] [(Int, Int)]
+choose :: MonadPlus m => [a] -> m a
+choose = msum . map return
+
+addQueens :: MonadPlus m => Int -> Int -> [(Int, Int)] -> m [(Int, Int)]
 addQueens _ 0 qs = return qs
 addQueens n col qs = do
-    row <- amb (safePositionsOnColumn n col qs)
+    row <- choose (safePositionsOnColumn n col qs)
     addQueens n (col - 1) ((row, col) : qs)
+
+instance Monad m => Alternative (ContT [r] m) where
+    empty = ContT $ const (return [])
+    (ContT f) <|> (ContT g) = ContT $ \k ->
+        f k >>= \xs -> g k >>= \ys -> return (xs ++ ys)
+
+instance Monad m => MonadPlus (ContT [r] m)
 
 nQueens :: Int -> [[(Int, Int)]]
 nQueens n =
-    allValues (addQueens n n [])
+    runCont (addQueens n n []) (\x -> [x])
