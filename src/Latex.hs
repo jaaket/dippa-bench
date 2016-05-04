6c7b1f9c7f6cdf7e8491d3734c3bb4763c4972a2
Jaakko Ketola <jaaket@kapsi.fi>
2016-05-04 11:34:11 +0300

    latex export booktabs rules

--- src/Latex.hs
+++ src/Latex.hs
@@ -1,9 +1,11 @@
 module Latex where
 
-import           Criterion.Types (Report (..))
-import           Data.List       (intersperse, transpose)
-import qualified Data.Text       as T
+import           Criterion.Types        (Report (..))
+import           Data.List              (intersperse, transpose)
+import qualified Data.Text              as T
 import           Text.LaTeX
+import           Text.LaTeX.Base.Class
+import           Text.LaTeX.Base.Syntax
 
 import           Bench
 
@@ -12,13 +14,13 @@ exportTable :: BenchGroup Report -> T.Text
 exportTable = render . resultsToLatex
 
 resultsToLatex :: BenchGroup Report -> LaTeX
-resultsToLatex group = tabular Nothing tableSpec content
+resultsToLatex group = tabular Nothing tableSpec table
   where
     tableSpec = [CenterColumn, VerticalLine] ++ replicate numBenches CenterColumn
 
     numBenches = length (bgBenches group)
 
-    content = header <> lnbk <> body
+    table = toprule <> header <> lnbk <> midrule <> body <> lnbk <> bottomrule
 
     header = fromString (T.unpack $ bgXAxisName group)
            & foldr1 (&) (map (fromString . T.unpack . benchDescription) (bgBenches group))
@@ -32,3 +34,12 @@ resultsToLatex group = tabular Nothing tableSpec content
     rows = transpose (xValues : map (map snd . benchData . fmap getMean) (bgBenches group))
 
     xValues = map fst (benchData (head (bgBenches group)))
+
+toprule :: LaTeXC l => l
+toprule = fromLaTeX (TeXCommS "toprule")
+
+midrule :: LaTeXC l => l
+midrule = fromLaTeX (TeXCommS "midrule")
+
+bottomrule :: LaTeXC l => l
+bottomrule = fromLaTeX (TeXCommS "bottomrule")
