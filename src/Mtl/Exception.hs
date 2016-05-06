2dae36dcd9f540aa7eea0618f4c9e5e76f4a3638
Jaakko Ketola <jaaket@kapsi.fi>
2016-05-06 13:32:03 +0300

    mtl: Use runExcept instead of Either directly

--- src/Mtl/Exception.hs
+++ src/Mtl/Exception.hs
@@ -6,7 +6,7 @@ import           Control.Monad.Except
 
 
 exception :: Int -> Either Int Int
-exception n = foldM f 1 (replicate n 1 ++ [0])
+exception n = runExcept $ foldM f 1 (replicate n 1 ++ [0])
   where
     f _ x | x == 0 = throwError 0
     f acc x = return $! acc * x
