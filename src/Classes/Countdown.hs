cfbfb02cd0df153b9c65984536eb81b9b6c42f10
Jaakko Ketola <jaaket@kapsi.fi>
2016-06-21 10:50:43 +0300

    Eta reduce countdown for monad-classes

--- src/Classes/Countdown.hs
+++ src/Classes/Countdown.hs
@@ -7,7 +7,7 @@ import           Control.Monad.Classes.Run
 
 
 countdown :: Int -> Int
-countdown n = run $ evalStateStrict n go
+countdown = run . flip evalStateStrict go
   where
     go = do
         x <- get
